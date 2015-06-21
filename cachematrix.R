8adc7683c93b69ef0d163ba92d2fb106d001048f
Elisberg <th.elisberg@gmail.com>
2015-06-21 22:02:26 +0200

    Coursera programming assignment2

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,21 @@
 ## Put comments here that give an overall description of what your
 ## functions do
 
-## Write a short comment describing this function
+## Cahces the inverse of the matrix and returns the result.
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  chacheInverse <- solve(x)  
 }
 
 
-## Write a short comment describing this function
+## In  case the two matrices x and the global cacheMatrix - defined outside the scope of the function
+## are not equal the inverse of the matrix will be calculated and stored in the global variable cacheMatrix
+## otherwise the cache is returned
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  if (!identical(x, cacheMatrix)) {
+    cacheMatrix <<- makeCacheMatrix(x)
+  }
+  
+  return(cacheMatrix)  
 }
